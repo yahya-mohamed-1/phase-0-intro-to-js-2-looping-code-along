3a96095e893fbbfaedb912f8c4afad3c846ee8f8
Yahya Mohamed <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-07-03 19:18:59 +0300

    Complete assignment submission.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
